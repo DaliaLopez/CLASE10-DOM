bc99668fd1401d1f5648d9675460dc178815d6d5
Dalia Lopez Cordoba <1112046308@u.icesi.edu.co>
2025-04-11 17:11:53 -0500

    ULTIMA ACT

--- ejercicios/ejercicio4/script.js
+++ ejercicios/ejercicio4/script.js
@@ -1,13 +1,51 @@
 
 /**
- * Ejercicio 4. Agregar y Eliminar Elementos al DOM
- *  
- * 1. Seleccionar los elementos: contenedor y boton con los que se va a trabajar
- * 2. Crear un nuevo elemento parafo con createElement en el documento
- * 3. Agregar el texto al nuevo parafo creado
- * 4. Adicionar el elemento creado al contenedor selecccioando con appendChild
- * 5. El codigo anterior incluirlo en una nueva funcion crearParafo()
- * 6. Adicionar al boton un escuchador para el evento click que  llame la funcion
- *    crear_parrafo()
- * 
+ * Ejercicio 4. Jerarquia Elementos al DOM
+ * ---------------------------------------
+ * 1. Capturar los elementos: contenedor, parrafo1 y parrafo2 con queryselector
+ * 2. Ir al nodo padre del parrafo1 y mostrar su etiqueta con tagName
+ * 3. Mostrar los hijos que tiene el elemento contenedor
+ * 3. Mostrar cuantos hijos tiene el elemento contenedor
+ * 4. Mostrar el primer hijo de el contenedor con []
+ * 5. Mostrar el Primer y último hijo con firstElementChild y lastElementChild
+ * 6. Mostrar el texto del siguiente hermano de parrafo1 nextElementSibling
+ * 7. Mostrar el id del anterior hermano de parrafo2 previousElementSibling
+ * 8. Recorrer la lista de frutas (Nodos hijos) con ayuda del DOM 
  */
+
+  // Capturar los  elementos 
+
+let contenedor = document.querySelector ("#contenedor")
+let parrafo1 = document.querySelector ("#parrafo1")
+let parrafo2 = document.querySelector ("#parrafo2")
+
+  // Nodo padre
+
+console.log (parrafo1.parentNode.tagName)
+
+  // Hijos del contenedor
+
+console.log (contenedor.children [0], contenedor.children [2])
+
+  // Primer y último hijo
+
+console.log (contenedor.firstElementChild)
+console.log (contenedor.lastElementChild)
+
+  // Hermanos
+
+console.log ("Hermanos")
+console.log (parrafo1.nextElementSibling)
+console.log (parrafo2.previousElementSibling)
+
+  // Recorrer la lista de frutas con el DOM
+
+let frutas = document.querySelector("#frutas")
+console.log (frutas)
+
+let hijos = frutas.children
+console.log (hijos)
+
+for (const elemento of hijos)  {
+    console.log (elemento.textContent)
+}
